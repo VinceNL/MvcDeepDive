c6055b759f2ce6ca8e92c91171af75f60e9d8cbd
vvancampen <vvancampen@msn.com>
2024-08-28 21:22:05 +0200

    Add project files.

---
+++ MvcShop.Domain/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace MvcShop.Domain.Models
+{
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+        public required string Name { get; set; }
+        public required decimal Price { get; set; }
+
+        public Product()
+        {
+            ProductId = Guid.NewGuid();
+        }
+    }
+}
