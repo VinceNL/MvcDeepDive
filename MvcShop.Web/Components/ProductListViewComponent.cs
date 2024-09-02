5118833b9c2666c29b969b35299da7b1b82d8645
vvancampen <vvancampen@msn.com>
2024-09-02 21:34:00 +0200

    Custom Taghelper / ViewComponent added

---
+++ MvcShop.Web/Components/ProductListViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcShop.Domain.Models;
+using MvcShop.Infrastructure.Repositories;
+
+namespace MvcShop.Web.Components
+{
+    public class ProductListViewComponent(IRepository<Product> repository, ILogger<ProductListViewComponent> logger) : ViewComponent
+    {
+        private readonly IRepository<Product> _repository = repository;
+        private readonly ILogger<ProductListViewComponent> _logger = logger;
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            var products = _repository.GetAll();
+
+            _logger.LogInformation($"Found a total of {products.Count()} products");
+
+            return Task.FromResult<IViewComponentResult>(View(products));
+        }
+    }
+}
---
+++ MvcShop.Web/Views/Shared/Components/ProductList/Default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MvcShop.Web.Views.Shared.Components.ProductList
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
